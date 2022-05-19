a9ad83b1c8efc60a28be1fad5e940facf55cda67
jhg3410 <80373033+jhg3410@users.noreply.github.com>
2022-05-19 17:23:23 +0900

    #2 - DiffUtil item이 삭제되면 data의 position과 adapter(초기 생성)에서의 position이 일치하지않아 다른 item이 삭제되는 경우가 있다.(notifyItemRangeChanged 도 참고하면 좋다.) 나의 예제 같은 경우는 check 했을 때 누른 position 값이 불일치 하기에 다른 값들이 삭제가 되는데 이를 해결하는 쉬운 방법은 position 말고 recyclerView.bindingAdapterPosition 를 사용하는 것이다. 이는 초기가 아닌 삭제되고 난 후의 adapter의 item의 위치를 가져온다. 그래서 결국엔 checkBox 누른 애의 position 이다.

--- app/src/main/java/org/inu/recyclerview/MainActivity.kt
+++ app/src/main/java/org/inu/recyclerview/MainActivity.kt
@@ -1,44 +1,97 @@
 package org.inu.recyclerview
 
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    val recyclerView : RecyclerView by lazy {
+    val recyclerView: RecyclerView by lazy {
         findViewById(R.id.recyclerView)
     }
+
+    private val addButton: AppCompatButton by lazy {
+        findViewById(R.id.addButton)
+    }
+
+    private val removeButton: AppCompatButton by lazy {
+        findViewById(R.id.removeButton)
+    }
+
+
+    private lateinit var mainAdapter: MainAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val data = listOf(
-            TodoData("운동하기",false),
-            TodoData("샤워하기",false),
-            TodoData("백준 문제 풀기",false),
-            TodoData("RecyclerView 공부하기",false),
-            TodoData("운동하기",false),
-            TodoData("샤워하기",false),
-            TodoData("백준 문제 풀기",false),
-            TodoData("RecyclerView 공부하기",false),
-            TodoData("운동하기",false),
-            TodoData("샤워하기",false),
-            TodoData("백준 문제 풀기",false),
-            TodoData("RecyclerView 공부하기",false),
-            TodoData("운동하기",false),
-            TodoData("샤워하기",false),
-            TodoData("백준 문제 풀기",false),
-            TodoData("RecyclerView 공부하기",false),
+
+        setData().let {
+            setRecyclerView(it)
+            setAddButton(it)
+            setRemoveButton(it)
+        }
+    }
+
+
+    private fun setData(): MutableList<TodoData> {
+        return mutableListOf(
+            TodoData("운동하기", false),
+            TodoData("샤워하기", false),
+            TodoData("백준 문제 풀기", false),
+            TodoData("RecyclerView 공부하기", false),
+            TodoData("운동하기", false),
+            TodoData("샤워하기", false),
+            TodoData("백준 문제 풀기", false),
+            TodoData("RecyclerView 공부하기", false),
+            TodoData("운동하기", false),
+            TodoData("샤워하기", false),
+            TodoData("백준 문제 풀기", false),
+            TodoData("RecyclerView 공부하기", false),
+            TodoData("운동하기", false),
+            TodoData("샤워하기", false),
+            TodoData("백준 문제 풀기", false),
+            TodoData("RecyclerView 공부하기", false),
         )
+    }
+
+    private fun setRecyclerView(data: MutableList<TodoData>) {
         val customDecoration = MainDecoration(10f, 30f, Color.GRAY)
-        recyclerView.addItemDecoration(customDecoration)
+        mainAdapter = MainAdapter(data)
+        with(recyclerView) {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            addItemDecoration(customDecoration)
+            adapter = mainAdapter
+        }
+    }
 
+    private fun setAddButton(data: MutableList<TodoData>) {
+        addButton.setOnClickListener {
+            val newData = mutableListOf<TodoData>().apply {
+                data.forEach{
+                    this.add(it)
+                }
+                    add(TodoData("새롭게 추가된 데이터!",false))
+            }
+            mainAdapter.update(newData)
+        }
+    }
 
-        recyclerView.adapter = MainAdapter(data)
-        recyclerView.layoutManager = LinearLayoutManager(this)
+    private fun setRemoveButton(data: MutableList<TodoData>) {
+        removeButton.setOnClickListener {
+            val newData = mutableListOf<TodoData>().apply {
+                data.forEach{
+                    if (!it.done) {
+                        this.add(it)
+                    }
+                    mainAdapter.notifyItemRangeChanged(data.indexOf(it),1)
+                }
+            }
+            mainAdapter.update(newData)
+        }
     }
 }
--- app/src/main/java/org/inu/recyclerview/MainAdapter.kt
+++ app/src/main/java/org/inu/recyclerview/MainAdapter.kt
@@ -5,9 +5,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
-class MainAdapter(private val data: List<TodoData>): RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+class MainAdapter(private val data: MutableList<TodoData>): RecyclerView.Adapter<MainAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val todo:TextView = view.findViewById(R.id.itemTextView)
         val done:CheckBox = view.findViewById(R.id.checkBox)
@@ -21,8 +22,10 @@ class MainAdapter(private val data: List<TodoData>): RecyclerView.Adapter<MainAd
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.todo.text = data[position].todo
         holder.done.isChecked = data[position].done
-        doneClick(holder.done,position)
+        doneClick(holder.done,holder.bindingAdapterPosition)
     }
+
+    override fun getItemCount() = data.size
 
     private fun doneClick(done:CheckBox,position:Int){
         done.setOnClickListener {
@@ -30,5 +33,25 @@ class MainAdapter(private val data: List<TodoData>): RecyclerView.Adapter<MainAd
         }
     }
 
-    override fun getItemCount() = data.size
+    fun update(newData: MutableList<TodoData>) {
+        val diffResult = DiffUtil.calculateDiff(ContentDiffUtil(data,newData))
+        data.clear()
+        data.addAll(newData)
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+
+    class ContentDiffUtil(private val oldList: List<TodoData>, private val currentList: List<TodoData>) : DiffUtil.Callback() {
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition] == currentList[newItemPosition]
+        }
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].todo == currentList[newItemPosition].todo
+        }
+
+        override fun getOldListSize(): Int = oldList.size
+
+        override fun getNewListSize(): Int = currentList.size
+    }
 }
